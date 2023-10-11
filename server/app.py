043ccb98817e75b126a7b9b8180fc5717882b9fe
Frank Dillon <dillonf2@outlook.com>
2023-10-11 15:01:13 -0400

    building API mostly complete

--- server/app.py
+++ server/app.py
@@ -2,16 +2,14 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder = None
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -20,19 +18,69 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+
+    bakery_list = [
+        {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at,
+            'updated_at': bakery.updated_at
+        }
+        for bakery in bakeries
+    ]
+    return jsonify(bakery_list), 200
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery(id):
+    bakery = Bakery.query.get(id)
+
+    if bakery:
+        bakery_data = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at.isoformat() if bakery.created_at else None,
+            'updated_at': bakery.updated_at.isoformat() if bakery.updated_at else None,
+            'baked_goods': [
+                {
+                    'id': good.id,
+                    'name': good.name,
+                    'price': good.price,
+                    'created_at': good.created_at.isoformat() if good.created_at else None,
+                    'updated_at': good.updated_at.isoformat() if good.updated_at else None
+                } 
+                for good in bakery.baked_goods
+            ]
+        }
+        return jsonify(bakery_data), 200
+    else:
+        return jsonify({'message': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [
+        {
+            'id': good.id,
+            'name': good.name,
+            'price': good.price,
+            'created_at': None,
+        }
+        for good in baked_goods
+    ]
+
+    return jsonify(baked_goods_list), 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if most_expensive_good:
+        most_expensive_data = most_expensive_good.to_dict()
+        return jsonify(most_expensive_data), 200
+    else:
+        return jsonify({'message': 'No baked goods found.'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    print(f'The thing I need: {jsonify(baked_goods_list), 200}')
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,13 +9,42 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Bakery(db.Model, SerializerMixin):
+class Bakery(db.Model):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-class BakedGood(db.Model, SerializerMixin):
+    baked_goods = db.relationship('BakedGood', back_populates='bakery')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
+            'baked_goods': [baked_good.to_dict() for baked_good in self.baked_goods]
+        }
+
+class BakedGood(db.Model):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    bakery = db.relationship('Bakery', back_populates='baked_goods')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
+        }
